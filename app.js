ab87c556c9ad436f0bd0f1c3bcc1a1e06859a8a3
revoo <rusr@protonmail.com>
2020-05-15 17:51:28 -0500

    Add node server

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// PaaS service like Heroku will assign us a port, if running locally use 5000.
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => res.sendFile('./index.html', {root: __dirname}));
+
+// serve the react JS file when GET requested
+app.get('/bundle.js', (req, res) => res.sendFile('./dist/bundle.js', {root: __dirname}));
+
+app.listen(PORT);
+
+console.debug(`Node server running and listening on port: ${PORT}`)
